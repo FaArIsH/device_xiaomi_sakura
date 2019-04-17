5e0ec33c38ce26b553df2c617250c7f654cdda11
FaArIsH <faarishhassan@gmail.com>
2019-04-17 13:42:18 +0500

    change fingerprint to daisy

--- revengeos_sakura.mk
+++ revengeos_sakura.mk
@@ -43,10 +43,10 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
+    PRIVATE_BUILD_DESC="daisy-user 9 PKQ1.180917.001 V10.0.3.0.PDLMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/daisy/daisy_sprout:9/PKQ1.180917.001/V10.0.3.0.PDLMIXM:user/release-keys"
 
 # Use Gcam and Jelly
 TARGET_USE_GCAM := true
